0e64026adf11ad9c4e0af86f3549d6711e21ca91
Steven Israel <malenchite@gmail.com>
2021-02-15 10:27:14 -0500

    Implemented GET and POST for api/notes

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const fs = require("fs");
+const uniqid = require("uniqid");
 
 /* Set up express app */
 const app = express();
 const PORT = process.env.PORT || 5000;
+const DBPATH = path.join(__dirname, "db", "db.json");
 
 /* Middleware */
 app.use(cors());
@@ -14,10 +16,30 @@ app.use(express.json());
 /* Public static directory for serving up assets and files */
 app.use(express.static(path.join(__dirname, "public")));
 
-/* Routes */
+/* GET Routes */
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public", "notes.html")));
 
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "db", "db.json"));
+});
+
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
+
+/* POST Routes */
+
+/* API to save a new note */
+app.post("/api/notes", (req, res) => {
+  let newNote = req.body;
+  let noteList = JSON.parse(fs.readFileSync(DBPATH));
+
+  /* Create unique ID and add new note to DB */
+  newNote.id = uniqid();
+  noteList.push(newNote);
+  fs.writeFileSync(DBPATH, JSON.stringify(noteList));
+
+  /* Return new note */
+  res.json(newNote);
+});
 
 
 /* Start server */
