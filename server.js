a54c58c25a137f5a1a5b6547dd627e7e3df345c5
Steven Israel <malenchite@gmail.com>
2021-02-15 09:52:00 -0500

    Routes for static HTML, CSS, JS files

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+/* Public static directory for serving up assets and files */
+app.use(express.static(path.join(__dirname, "public")));
+
+/* Routes */
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public", "notes.html")));
+
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
+
 
 /* Start server */
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
